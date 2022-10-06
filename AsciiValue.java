18e8187639889aa2bcf68fbe57edb2fcb092d87c
christopherjoyjayakumari <112762701+christopherjoyjayakumari@users.noreply.github.com>
2022-10-06 13:05:48 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,15 @@
+public class AsciiValue {
+
+	public static void main(String[] args) {		char ch='H';
+
+		int ascii=ch;
+
+		int casAscii=(int) ch;
+
+		System.out.println("The ASCII value of" + ch +" is:" + ascii);
+
+		System.out.println("The ASCII value of " + ch + " is:" + casAscii);
+
+	}
+
+}
